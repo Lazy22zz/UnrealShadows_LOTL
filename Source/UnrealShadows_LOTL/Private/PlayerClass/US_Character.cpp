d8ec803be051cad8e98b4d8f15e112552915b5c3
Lazy22zz <claudeleave@gmail.com>
2025-11-13 09:57:31 -0800

    Add enhancedinput functions

--- Source/UnrealShadows_LOTL/Private/PlayerClass/US_Character.cpp
+++ Source/UnrealShadows_LOTL/Private/PlayerClass/US_Character.cpp
@@ -4,8 +4,13 @@
 #include "PlayerClass/US_Character.h"
 #include "Camera/CameraComponent.h"
 #include "Components/CapsuleComponent.h"
+#include "Components/InputComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/Controller.h"
+
 
 // Sets default values
 AUS_Character::AUS_Character()
@@ -45,6 +50,56 @@ void AUS_Character::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
+}
+
+void AUS_Character::Move(const FInputActionValue& Value)
+{
+	const auto MovementVector = Value.Get<FVector2D>();
+	GEngine->AddOnScreenDebugMessage(0, 5.f, FColor::Yellow, FString::Printf(TEXT("MovementVector: %s"), *MovementVector.ToString()));
+	if (Controller != nullptr)
+	{
+		const auto Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+		const auto ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		const auto RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		AddMovementInput(ForwardDirection, MovementVector.Y);
+		AddMovementInput(RightDirection, MovementVector.X);
+	}
+}
+
+void AUS_Character::Look(const FInputActionValue& Value)
+{
+	const auto LookAxisVector = Value.Get<FVector2D>();
+	GEngine->AddOnScreenDebugMessage(1, 5.f, FColor::Green,FString::Printf(TEXT("LookAxisVector: %s"), *LookAxisVector.ToString()));
+	if (Controller != nullptr)
+	{
+		AddControllerYawInput(LookAxisVector.X);
+		AddControllerPitchInput(LookAxisVector.Y);
+	}
+}
+
+void AUS_Character::SprintStart(const FInputActionValue& Value)
+{
+	GEngine->AddOnScreenDebugMessage(2, 5.f, FColor::Blue, TEXT("SprintStart"));
+	GetCharacterMovement()->MaxWalkSpeed = 3000.f;
+}
+
+void AUS_Character::SprintEnd(const FInputActionValue& Value)
+{
+	GEngine->AddOnScreenDebugMessage(2, 5.f, FColor::Blue, TEXT("SprintEnd"));
+	GetCharacterMovement()->MaxWalkSpeed = 500.f;
+}
+
+void AUS_Character::Interact(const FInputActionValue& Value)
+{
+	GEngine->AddOnScreenDebugMessage(3, 5.f, FColor::Red, TEXT("Interact"));
 }
 
 // Called every frame
@@ -59,6 +114,13 @@ void AUS_Character::SetupPlayerInputComponent(UInputComponent* PlayerInputCompon
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
-
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AUS_Character::Move);
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &AUS_Character::Look);
+		EnhancedInputComponent->BindAction(InteractAction, ETriggerEvent::Started, this, &AUS_Character::Interact);
+		EnhancedInputComponent->BindAction(SprintAction, ETriggerEvent::Started, this, &AUS_Character::SprintStart);
+		EnhancedInputComponent->BindAction(SprintAction, ETriggerEvent::Completed, this, &AUS_Character::SprintEnd);
+	}
 }
 
--- Source/UnrealShadows_LOTL/Public/PlayerClass/US_Character.h
+++ Source/UnrealShadows_LOTL/Public/PlayerClass/US_Character.h
@@ -11,6 +11,22 @@ class UNREALSHADOWS_LOTL_API AUS_Character : public ACharacter
 {
 	GENERATED_BODY()
 
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UInputMappingContext> DefaultMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UInputAction> MoveAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UInputAction> LookAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UInputAction> SprintAction;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UInputAction> InteractAction;
+
 public:
 	// Sets default values for this character's properties
 	AUS_Character();
@@ -18,6 +34,12 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+    
+	void Move(const struct FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+	void SprintStart(const FInputActionValue& Value);
+	void SprintEnd(const FInputActionValue& Value);
+	void Interact(const FInputActionValue& Value);
 
 public:	
 	// Called every frame
